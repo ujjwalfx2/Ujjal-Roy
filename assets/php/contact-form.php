12541e6c8ec53b5508fe31dd909ef95f1669f651
Ujjwal Roy <89298493+ujjwalfx2@users.noreply.github.com>
2021-09-07 02:42:21 +0530

    Update contact-form.php

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -17,7 +17,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "ujjwalfx2@gmail.com"; // Add your email address inbetween the replacing email@mail.com - This is where the form will send a message to.
 $email_subject = "Github-Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName:\n$name\n\nEmail:\n$email_address\n\nMessage:\n$message";
-$headers = "ujjwalfx2@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "noreply@yourdomain.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 
