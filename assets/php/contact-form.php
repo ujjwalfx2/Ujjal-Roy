7d7128d42103980fd4e7fdc2b2826ec7607bb94d
Ujjwal Roy <89298493+ujjwalfx2@users.noreply.github.com>
2021-09-07 02:40:20 +0530

    Add files via upload

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])  		||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
-	return false;
+	return true;
    }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -14,11 +14,12 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'email@mail.com'; // Add your email address inbetween the replacing email@mail.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = "ujjwalfx2@gmail.com"; // Add your email address inbetween the replacing email@mail.com - This is where the form will send a message to.
+$email_subject = "Github-Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName:\n$name\n\nEmail:\n$email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "ujjwalfx2@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
+
 return true;
 ?>
